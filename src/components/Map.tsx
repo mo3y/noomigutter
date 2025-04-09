1391863e301388421185dc62d41ee7babfc73ddc
Musa <musaallami@musas-MacBook-Air.local>
2025-04-08 20:43:28 -0400

    Fix favicon and remove Vite icon

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -4,9 +4,8 @@ const Map: React.FC = () => {
   return (
     <div className="relative h-[600px] rounded-xl overflow-hidden">
       <iframe
-        src="https://www.google.com/maps/d/u/0/embed?mid=1p4aQCWGsXv7vvWkatSkObJDqjWcKlUw&ehbc=2E312F"
-        width="100%"
-        height="100%"
+        src="https://www.google.com/maps/d/u/3/embed?mid=1_jsbZ61wujjHM6vMJymJeXVqboTE2rg&ehbc=2E312F" width="100%" height="100%"
+        
         style={{ border: 0 }}
         allowFullScreen
         loading="lazy"
